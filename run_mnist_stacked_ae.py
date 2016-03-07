3ffadd5ca4b556c0da8ee5ef8c94ed71cf574c5e
Sumanyu Sharma <sumanyu@hotmail.com>
2016-03-07 15:15:33 -0500

    Have two files, one for AE and one for stacked AE

--- run_mnist_stacked_ae.py
+++ run_mnist_stacked_ae.py
@@ -17,31 +17,6 @@ X, y = mnist.data / 255., mnist.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=42)
 
-
-# da = DenoisingAutoencoder(n_hidden=400, verbose=True, training_epochs=5)
-# da.fit(X_train)
-#
-# X_train_latent = da.transform_latent_representation(X_train)
-# X_test_latent = da.transform_latent_representation(X_test)
-#
-# clf = MultinomialNB()
-#
-# # Fit the model
-# clf.fit(X_train_latent, y_train)
-#
-# # Perform the predictions
-# y_predicted = clf.predict(X_test_latent)
-#
-# from sklearn.metrics import accuracy_score
-# print "Accuracy = {} %".format(accuracy_score(y_test, y_predicted)*100)
-#
-# from sklearn.metrics import classification_report
-# print "Classification Report \n {}".format(classification_report(y_test, y_predicted, labels=range(0,10)))
-#
-
-
-
-
 # Stacked AE test
 
 stacked_ae = StackedDenoisingAutoencoders(hidden_layers_sizes=[400, 200], verbose=True, training_epochs=5)
